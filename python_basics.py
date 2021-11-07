47072f16aa92a9029fa6e24d94cc599cc5bee316
AjuunWhiterhat <70275977+AjuunWhiterhat@users.noreply.github.com>
2021-11-07 12:44:15 +0530

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,10 @@
+pocketMoney = int(input("How much pocketmoney do you get?"))
+
+if(pocketMoney>500):
+    print("You get enough pocket money")
+
+elif(pocketMoney>100):
+    print("You have a good life")
+
+else:
+    print("I know what you feel")
